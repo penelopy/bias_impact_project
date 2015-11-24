47f4c6e7dea7f8eb202e5515a993df58e897e48d
Penelope Hill <penelope.codes@gmail.com>
2015-11-24 13:57:17 -0800

    Refactored simulator, data now showing 50/50

--- simulation.py
+++ simulation.py
@@ -1,6 +1,3 @@
-import math
-# import random
-import time
 from numpy import random
 
 from employee import Employee
@@ -10,24 +7,24 @@ from result import Result
 class Simulation:
 
     def __init__(self, num_simulations, attrition, iterations_per_simulation, promotion_bias, 
-        num_positions_list, bias_towards_gender):
+        num_positions_at_level, bias_favors_this_gender):
         self.num_simulations = num_simulations
         self.attrition = attrition
         self.iterations_per_simulation = iterations_per_simulation
         self.promotion_bias = promotion_bias
-        self.num_positions_list = num_positions_list
-        self.bias_towards_gender = bias_towards_gender
-        self.num_levels = len(num_positions_list)
+        self.num_positions_at_level = num_positions_at_level
+        self.bias_favors_this_gender = bias_favors_this_gender
+        self.num_employee_levels = len(num_positions_at_level)
 
         self.init_employees()
         self.hire()
 
     def init_employees(self):
-        """Build up mapping of level to list of empty array, which will
+        """Build up mapping of levels with an empty array, which will
         eventually be populated with Employees"""
 
         self.levels_to_employees = {}
-        for i in range(0, self.num_levels):
+        for i in range(0, self.num_employee_levels):
             self.levels_to_employees[i] = []
 
     def hire(self):
@@ -36,7 +33,7 @@ class Simulation:
         #randomly assign gender to first employee
         next_gender = random.choice(gender)
         level = 0
-        for positions in self.num_positions_list:
+        for positions in self.num_positions_at_level:
             employee_list_at_level = self.levels_to_employees.get(level)
             append = employee_list_at_level.append
             if employee_list_at_level is not None:
@@ -48,9 +45,8 @@ class Simulation:
                         next_gender = "female"
             level += 1
 
-
     def run(self):
-        """run simulation"""
+        """Run simulation"""
         for _ in xrange(0, self.iterations_per_simulation):
             self.talent_review()
             self.attrit()
@@ -63,16 +59,17 @@ class Simulation:
         bias = 100 + self.promotion_bias
         for employee_list in self.levels_to_employees.values(): 
             for employee in employee_list:
-                if employee.gender == self.bias_towards_gender:
+                if employee.gender == self.bias_favors_this_gender:
                     employee.rating = random.randint(0, bias) 
                 else:
                     employee.rating = random.randint(0, 100)
+
 
     def attrit(self):
         """Looks at each employee in dictionary and randomly retains employees
         based on global attrition rate"""
 
-        for level in range(self.num_levels):
+        for level in range(self.num_employee_levels):
             employee_list_at_level = self.levels_to_employees.get(level)
             num_employees_at_level = len(employee_list_at_level)
             num_employees_to_retain = int(num_employees_at_level * ((100 - self.attrition)/100.0))
@@ -80,44 +77,46 @@ class Simulation:
             retained_employees = []
             for i in indices_to_retain: 
                 retained_employees.append(employee_list_at_level[i])
+
             self.levels_to_employees[level] = retained_employees
 
-
     def promote(self):
-        """Looks at each level, determines the number of promotions, adds and
-        deletes employees to various levels"""
-        for i in range(self.num_levels - 1):
-            prev_level = i
-            candidates = self.levels_to_employees.get(prev_level)
-            new_level = i + 1
-            targets = self.levels_to_employees.get(new_level)
+        """Starts at highest level and checks for open positions, then removes the top
+        employees from the level below to fill the open positions. Continues this process through 
+        each lower level. Only the entry level will have open positions at the end of this method."""
+        for i in range(self.num_employee_levels - 1, 0, -1):
+            promote_to_level = i
+            promote_from_level = i - 1
 
-            candidates.sort(key=lambda x: x.rating, reverse=True)
+            promote_from_employees = self.levels_to_employees.get(promote_from_level)
 
-            num_candidates = len(candidates)
-            open_positions = self.num_positions_list[new_level] - len(targets)
+            promote_to_employees = self.levels_to_employees.get(promote_to_level)
+
+            promote_from_employees.sort(key=lambda x: x.rating, reverse=True)
+
+            num_candidates = len(promote_from_employees)
+            total_positions = self.num_positions_at_level[promote_to_level]
+            fill_positions = len(promote_to_employees)
+            open_positions = total_positions - fill_positions
             num_promotions = min(num_candidates, open_positions)
-            candidates_to_promote = candidates[:num_promotions]
+            candidates_to_promote = promote_from_employees[:num_promotions]
 
-            self.levels_to_employees[prev_level] = candidates[num_promotions:]
-            self.levels_to_employees[new_level] = targets + candidates_to_promote
-            candidates = self.levels_to_employees.get(prev_level)
-
-            targets = self.levels_to_employees.get(new_level)
+            self.levels_to_employees[promote_from_level] = promote_from_employees[num_promotions:]
+            self.levels_to_employees[promote_to_level] = promote_to_employees + candidates_to_promote
 
     def get_result(self):
         """Counts number of men and women at each level and saves totals to
-        num_men or num_women lists"""
-        num_men = [0] * self.num_levels
-        num_women = [0] * self.num_levels
+        the corresponding list."""
+        total_men_at_level = [0] * self.num_employee_levels
+        total_women_at_level = [0] * self.num_employee_levels
 
-        for level in range(self.num_levels):
+        for level in range(self.num_employee_levels):
             employee_list = self.levels_to_employees.get(level)
 
             for employee in employee_list:
                 if employee.gender == "male": 
-                    num_men[level] += 1
+                    total_men_at_level[level] += 1
                 else:
-                    num_women[level] += 1
+                    total_women_at_level[level] += 1
 
-        return Result(num_men, num_women)
+        return Result(total_men_at_level, total_women_at_level)
